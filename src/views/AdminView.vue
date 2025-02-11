b0fdcd64b007142098387e391d1b21739b4fe6d2
Raul Rodriguez <rrodfer05@gmail.com>
2025-02-11 19:22:47 +0100

    ADMIN TERMINADO :-)

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -32,6 +32,19 @@ async function actualizarRol(usuarioId, nuevoRol) {
     error.value = err.message;
   }
 }
+
+async function eliminarUsuario(usuarioId) {
+  try{
+    const response = await fetch("http://localhost/APIFreetours/api.php/usuarios?id="+ usuarioId,{
+      method: "DELETE",
+    });
+
+    if (!response.ok) throw new Error("Error al eliminar el usuario");
+    obtenerUsuarios()
+  }catch (err) {
+        error.value = err.message;
+    }
+}
 obtenerUsuarios(); // Recargar usuarios para reflejar los cambios
 // Cargar usuarios al montar el componente
 //onMounted(obtenerUsuarios);
@@ -43,14 +56,15 @@ obtenerUsuarios(); // Recargar usuarios para reflejar los cambios
     <p v-if="error" class="text-danger">{{ error }}</p>
     <table class="table table-striped mb-5 mt-3">
       <thead>
-        <tr>
+        <tr class="text-center">
           <th>ID</th>
           <th>Nombre</th>
           <th>Rol</th>
+          <th>Eliminar Usuario</th>
         </tr>
       </thead>
       <tbody>
-        <tr  v-for="usuario in usuarios" :key="usuario.id" >
+        <tr class="text-center"  v-for="usuario in usuarios" :key="usuario.id" >
           <td>{{ usuario.id }}</td>
           <td>{{ usuario.nombre }}</td>
           <td>
@@ -58,8 +72,23 @@ obtenerUsuarios(); // Recargar usuarios para reflejar los cambios
               <option v-for="rol in rolesDisponibles" :key="rol" :value="rol">{{ rol }}</option>
             </select>
           </td>
+          <td><button class="btn-delete col-7 mt-1 p-2" @click="eliminarUsuario(usuario.id)">❌ Eliminar</button></td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
+<style scoped>
+.btn-delete {
+    background-color: #ff4d4d;
+    color: white;
+    border: none;
+    cursor: pointer;
+    border-radius: 5px;
+    transition: background 0.3s ease;
+}
+
+.btn-delete:hover {
+    background-color: #cc0000;
+}
+</style>
